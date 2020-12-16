27c7bd7995f3b0639049763150317af6ae0e11f2
Anton Strogonoff <anton@strogonoff.name>
2020-12-16 14:09:39 +0900

    chore: bump to newer extension & registry kits

--- src/plugin.ts
+++ src/plugin.ts
@@ -2,6 +2,9 @@ import { makeExtension } from '@riboseinc/paneron-extension-kit';
 import 'electron';
 
 export default makeExtension({
-  repoView: () => import('./RepoView'),
+  mainView: () => import('./RepoView'),
   name: "Glossarist",
-})
+  requiredHostAppVersion: '^1.0.0-alpha19',
+  datasetMigrations: {},
+  datasetInitializer: () => import('./migrations/initial'),
+});
