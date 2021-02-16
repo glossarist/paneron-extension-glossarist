44dcd8a03059c5e1d4897a8928efaefb53b8337d
Anton Strogonoff <anton@strogonoff.name>
2021-02-16 10:59:24 +0900

    fix: update abstract concept item class definition

--- src/classes/concept.tsx
+++ src/classes/concept.tsx
@@ -13,11 +13,8 @@ import { languageTitles, SupportedLanguage } from '../models/lang';
 export interface ConceptData {
   identifier: string
 
-  // These strings are UUIDs
-  designations: {
-    [languageCode: string]: string[]
-  }
-  definitions: {
+  // These point to UUIDs
+  localizedConcepts: {
     [languageCode: string]: string
   }
 };
@@ -35,40 +32,19 @@ export const concept: ItemClassConfiguration<ConceptData> = {
   views: {
     listItemView: (props) => <span className={props.className}>{props.itemData.identifier}</span>,
     detailView: ({ itemData, useRegisterItemData, getRelatedItemClassConfiguration }) => {
-      const designationsByLang = Object.entries(itemData.designations);
-      const definitionsByLang = Object.entries(itemData.definitions);
+      const localizedConcepts = Object.entries(itemData.localizedConcepts);
 
       return (
         <div>
-          <H3 css={css`margin-top: 1.5em`}>Designations</H3>
+          <H3 css={css`margin-top: 1.5em`}>Localized concepts</H3>
 
           <UL css={css`padding-left: 0; list-style: square;`}>
-            {designationsByLang.map(([langID, designations]) =>
-              designations.map((uuid, idx) =>
-                <li key={`${langID}-${idx}`} css={css`margin-top: 1em;`}>
-                  <PropertyDetailView
-                      title={`Designation ${idx + 1}`}
-                      secondaryTitle={languageTitles[langID as SupportedLanguage]}>
-                    <GenericRelatedItemView
-                      itemRef={{ classID: 'designation', subregisterID: langID, itemID: uuid }}
-                      useRegisterItemData={useRegisterItemData}
-                      getRelatedItemClassConfiguration={getRelatedItemClassConfiguration}
-                    />
-                  </PropertyDetailView>
-                </li>
-              )
-            )}
-          </UL>
-
-          <H3 css={css`margin-top: 1.5em`}>Definition</H3>
-
-          <UL css={css`padding-left: 0; list-style: square;`}>
-            {definitionsByLang.map(([langID, uuid]) =>
+            {localizedConcepts.map(([langID, uuid]) =>
               <li key={langID} css={css`margin-top: 1em;`}>
                 <PropertyDetailView
                     title={`In ${languageTitles[langID as SupportedLanguage]}`}>
                   <GenericRelatedItemView
-                    itemRef={{ classID: 'definition', subregisterID: langID, itemID: uuid }}
+                    itemRef={{ classID: 'localized-concept', subregisterID: langID, itemID: uuid }}
                     useRegisterItemData={useRegisterItemData}
                     getRelatedItemClassConfiguration={getRelatedItemClassConfiguration}
                   />
