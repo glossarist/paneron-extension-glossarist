01bcd5440847d290eaf0b207df910e8de372497a
Anton Strogonoff <anton@strogonoff.name>
2021-02-22 13:50:23 +0000

    fix: make openLinkInBrowser() not break static site build

--- src/classes/localizedConcept/util.ts
+++ src/classes/localizedConcept/util.ts
@@ -1,3 +1,14 @@
 export function openLinkInBrowser(link: string) {
-  require('electron').shell.openExternal(link);
+  try {
+    const el = require.resolveWeak
+      ? __webpack_modules__[require.resolveWeak('electron') as number]
+      : undefined;
+    if (el) {
+      el.shell.openExternal(link);
+    }
+  } catch (e) {
+    console.error("Unable to open link in default browser", link, e);
+    return;
+  }
+
 }
