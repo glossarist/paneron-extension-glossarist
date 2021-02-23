d617ef242c7cfb592bbd960400ed84fe0aafe424
Anton Strogonoff <anton@strogonoff.name>
2021-02-23 04:21:04 +0000

    chore: make openLinkInBrowser work in browser

--- src/classes/localizedConcept/util.ts
+++ src/classes/localizedConcept/util.ts
@@ -1,14 +1,19 @@
 export function openLinkInBrowser(link: string) {
-  try {
-    const el = require.resolveWeak
-      ? __webpack_modules__[require.resolveWeak('electron') as number]
-      : undefined;
-    if (el) {
-      el.shell.openExternal(link);
+  // Some dance in an attempt to detect Electron without inadvertently including it in a bundle
+  if (typeof require !== 'undefined' && require.resolveWeak !== undefined) {
+    try {
+      const el = __webpack_modules__[require.resolveWeak('electron') as number];
+      if (el) {
+        el.shell.openExternal(link);
+      }
+    } catch (e) {
+      console.error("Unable to open link in default browser", link, e);
     }
-  } catch (e) {
-    console.error("Unable to open link in default browser", link, e);
-    return;
+  } else if (typeof window !== 'undefined') {
+    try {
+      window.location.assign(link);
+    } catch (e) {
+      console.error("Unable to navigate to link", link, e);
+    }
   }
-
 }
