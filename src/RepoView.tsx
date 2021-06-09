8f8d96e0f07cc45770bbaf625e3c78193949a1cb
Anton Strogonoff <anton@strogonoff.name>
2021-06-09 13:53:10 +0300

    chore: specify defaultSearchCriteria for RegistryView

--- src/RepoView.tsx
+++ src/RepoView.tsx
@@ -10,7 +10,16 @@ Object.assign(console, log);
 
 
 import { RegistryView } from '@riboseinc/paneron-registry-kit/views';
+import CRITERIA_CONFIGURATION from '@riboseinc/paneron-registry-kit/views/FilterCriteria/CRITERIA_CONFIGURATION';
 import { itemClassConfiguration, subregisters } from './registryConfig';
+
+
+
+const defaultClassID = Object.keys(itemClassConfiguration)[0];
+const defaultCriteria = CRITERIA_CONFIGURATION['item-class'].toQuery(
+  { classID: defaultClassID },
+  { itemClasses: itemClassConfiguration },
+);
 
 
 const RepositoryView: React.FC<Record<never, never>> =
@@ -19,6 +28,7 @@ function () {
     itemClassConfiguration={itemClassConfiguration}
     subregisters={subregisters}
     keyExpression="obj.data.identifier || `${obj.data.terms?.[0]?.designation}-${obj.id}` || obj.id"
+    defaultSearchCriteria={{ require: 'all', criteria: [{ key: 'item-class', query: defaultCriteria }] }}
   />
 };
 
