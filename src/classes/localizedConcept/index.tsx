f8651fb0485acb1a6007d1b8fe1308d70af005c5
Anton <anton@strogonoff.name>
2023-04-21 15:57:26 +0800

    feat(localized-concept): show PoS in item view
    (where available)

--- src/classes/localizedConcept/index.tsx
+++ src/classes/localizedConcept/index.tsx
@@ -4,10 +4,24 @@
 import React from 'react';
 import { jsx, css } from '@emotion/react';
 import type { ItemClassConfiguration } from '@riboseinc/paneron-registry-kit/types';
+import type { Designation } from '../../models/concepts';
 import { defaultLanguage, type WritingDirectionality, writingDirectionalityOverrides } from '../../models/lang';
 import LocalizedConceptForm from './LocalizedConceptForm';
 import LocalizedConceptDetails from './LocalizedConceptDetails';
 import LocalizedConceptData from './LocalizedConceptData';
+
+
+const PrimaryDesignation: React.FC<{ term: Designation | undefined }> =
+function ({ term }) {
+  if (!term) {
+    return <>(no designation)</>;
+  }
+  let suffix = '';
+  if (term.type === 'expression' && term.partOfSpeech) {
+    suffix += ` (${term.partOfSpeech})`;
+  }
+  return <>{term.designation}{suffix}</>;
+}
 
 
 export const localizedConcept: ItemClassConfiguration<LocalizedConceptData> = {
@@ -28,13 +42,9 @@ export const localizedConcept: ItemClassConfiguration<LocalizedConceptData> = {
   },
   itemSorter: () => 0,
   views: {
-    listItemView: (props) => {
-      return (
-        <span className={props.className}>
-          {props.itemData?.terms?.[0]?.designation ?? '<no designation>'}
-        </span>
-      );
-    },
+    listItemView: (props) => <span className={props.className}>
+      <PrimaryDesignation term={props.itemData?.terms?.[0]} />
+    </span>,
     detailView: (props) => <LocalizedConceptDetails
       css={css`padding: 1rem; position: absolute; inset: 0;`}
       localizedConcept={props.itemData}
