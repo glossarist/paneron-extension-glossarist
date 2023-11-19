7adb396d78392c5c7071ba743f6239c4cc896914
Anton <anton@strogonoff.name>
2023-11-19 23:26:47 +0800

    refactor: switch to makeRegistryExtension

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,11 +1,22 @@
-import { makeExtension } from '@riboseinc/paneron-extension-kit';
-import datasetInitializer from '@riboseinc/paneron-registry-kit/migrations/initial';
-import mainView from './RepoView';
+import { makeRegistryExtension, CRITERIA_CONFIGURATION } from '@riboseinc/paneron-registry-kit';
 
-export default makeExtension({
+import { itemClassConfiguration } from './registryConfig';
+
+
+const defaultClassID = Object.keys(itemClassConfiguration)[0];
+const defaultCriteria = CRITERIA_CONFIGURATION['item-class'].toQuery(
+  { classID: defaultClassID },
+  { itemClasses: itemClassConfiguration },
+);
+
+const defaultSearchCriteria = {
+  require: 'all',
+  criteria: [{ key: 'item-class', query: defaultCriteria }],
+} as const;
+
+export default makeRegistryExtension({
   name: "Glossarist",
-  requiredHostAppVersion: '2.0.0',
-  mainView,
-  datasetInitializer,
-  datasetMigrations: {},
+  itemClassConfiguration,
+  defaultSearchCriteria: defaultSearchCriteria as any,
+  keyExpression: "obj.data.identifier || `${obj.data.terms?.[0]?.designation}-${obj.id}` || obj.id",
 });
