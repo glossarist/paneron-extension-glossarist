4db140347e66a3d1c37331961c5ab7d51aebd748
Anton <anton@strogonoff.name>
2023-12-06 19:47:39 +0800

    feat: provide custom quick search predicate func

--- src/plugin.ts
+++ src/plugin.ts
@@ -14,9 +14,21 @@ const defaultSearchCriteria = {
   criteria: [{ key: 'item-class', query: defaultCriteria }],
 } as const;
 
+function getQuickSearchPredicate(quickSearchString: string) {
+  return `[
+    ...(obj.data?.terms || []).map(t => t.designation?.toString()),
+    ...(obj.data?.definition || []).map(d => d.content?.toString()),
+    ...(obj.data?.notes || []).map(d => d.content?.toString()),
+    ...(obj.data?.examples || []).map(e => e.content?.toString()),
+    ...(obj.data?.authoritativeSource || []).map(s => s.ref?.toString()),
+    obj.data?.identifier?.toString() ?? '',
+  ].join('').toLowerCase().indexOf("${quickSearchString.toLowerCase()}") >= 0`;
+}
+
 export default makeRegistryExtension({
   name: "Glossarist",
   itemClassConfiguration,
   defaultSearchCriteria: defaultSearchCriteria as any,
   keyExpression: "obj.data.identifier || `${obj.data.terms?.[0]?.designation}-${obj.id}` || obj.id",
+  getQuickSearchPredicate,
 });
